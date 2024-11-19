0fc42598a7e86a83deda9525024727af8a3ead4e
Eric Zou <ezou626@gmail.com>
2024-11-18 21:11:16 -0500

    add improved prompt and check to aggregation

--- src/agg.py
+++ src/agg.py
@@ -14,17 +14,23 @@ class Aggregation:
         self.qc = QualityControl(image_path, 80)
         self.valid_transcriptions = []
 
-    def check(self):
+    def check(self) -> bool:
         return len(self.valid_transcriptions) >= 3
 
-    def process_transcription(self, transcription):
+    def process_transcription(self, transcription) -> None:
         if self.qc.fits_quality_control(transcription):
             self.valid_transcriptions.append(transcription)
     
-    def aggregate(self):
+    def aggregate(self) -> str:
+        if not self.check():
+            return ""
         n = len(self.valid_transcriptions)
-        joined = "\n".join([f"Caption {i + 1}: {x}" for i, x in enumerate(self.valid_transcriptions)])
-        message_prompt = f"Your job is to combine the following {n} image captions, keeping as much information as possible from each but also keeping information consistent (there may be errors in each): {joined}"
+        joined = "\n".join([f"Caption {i + 1}: {x}\n" for i, x in enumerate(self.valid_transcriptions)])
+        message_prompt = f"""Your job is to combine the following {n} image captions into 1 unified description capturing all of the information in each.
+        Please do your best to keep as many of the details as possible while maintaining consistency of the scene.
+        Remove phrases and words that do not make sense in the context provided by the responses. Please do not report anything else. Only return the description.
+        {joined}
+        Please provide the resulting description in your following message:"""
         chat_completion = client.chat.completions.create(
             messages=[
                 {
@@ -35,23 +41,5 @@ class Aggregation:
             model="llama3.1-8b",
             max_tokens=8192,
         )
-        result_one = chat_completion.choices[0].message.content
-        result_two = chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": message_prompt
-                },
-                {
-                    "role": "assistant",
-                    "content": result_one
-                },
-                {
-                    "role": "user",
-                    "content": "Now refine the wording so it is more logical."
-                },
-            ],
-            model="llama3.1-8b",
-            max_tokens=8192,
-        ).choices[0].message.content
-        return result_two
+        result = chat_completion.choices[0].message.content
+        return result
