b85c39538aac53189567424c0cbd9aeae779610f
Eric Zou <ezou626@gmail.com>
2024-11-20 18:00:30 -0500

    add supabase client

--- src/clients.py
+++ src/clients.py
@@ -2,6 +2,9 @@ import os
 from openai import OpenAI
 from cerebras.cloud.sdk import Cerebras
 import whisper
+from mega import Mega
+from supabase import create_client, Client
+
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -10,4 +13,17 @@ OPENAI_CLIENT = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 CEREBRAS_CLIENT = Cerebras(api_key = os.environ.get('CEREBRAS_API_KEY'))
 
-WHISPER_MODEL = whisper.load_model("tiny.en" if os.environ.get('ENV_TYPE') == 'dev' else 'base.en')
+WHISPER_MODEL = lambda _: "This is a sample transcription" 
+if os.environ.get('ENV_TYPE') != 'dev':
+    whisper.load_model('base.en')
+    #whisper.load_model('tiny.en')
+
+MEGA_CLIENT = Mega().login(
+    os.environ.get('MEGA_USER'),
+    os.environ.get('MEGA_PASSWORD')
+)
+
+SUPABASE_CLIENT: Client = create_client(
+    os.environ.get('SUPABASE_URL'),
+    os.environ.get('SUPABASE_ADMIN_KEY')
+)
