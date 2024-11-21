e8be7246a599113b012f56639908f384f67239f6
Eric Zou <ezou626@gmail.com>
2024-11-21 07:52:06 -0500

    cha cha render smooth

--- windows_run.py
+++ windows_run.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     if os.environ.get('ENV_TYPE') in ['dev', 'test']:
         uvicorn.run("src.routes:app", reload=True, host="127.0.0.1", port=8000, log_level="info")
     else:
-        uvicorn.run("src.routes:app", host="127.0.0.1", port=10000, log_level="info")
+        uvicorn.run("src.routes:app", host="0.0.0.0", port=10000, log_level="info")
