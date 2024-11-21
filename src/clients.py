66064ac312a79c472ef6dbca91ec796f2d0153f1
Eric Zou <ezou626@gmail.com>
2024-11-21 05:55:25 -0500

    better mocking for dev and tiny model for test

--- src/clients.py
+++ src/clients.py
@@ -13,10 +13,19 @@ OPENAI_CLIENT = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 CEREBRAS_CLIENT = Cerebras(api_key = os.environ.get('CEREBRAS_API_KEY'))
 
-WHISPER_MODEL = lambda _: "This is a sample transcription" 
-if os.environ.get('ENV_TYPE') != 'dev':
-    whisper.load_model('base.en')
-    #whisper.load_model('tiny.en')
+class MockModel:
+    def __init__(self):
+        pass
+    def transcribe(self, _):
+        return {"text": "There is a man walking two dogs in a city park and it appears to be the middle of winter. One of the dogs is a small and black. The other one is a golden retriever. The person is wearing a grey hat and a scarf around. There knows a black coat, gloves and what appears to be snow pants. The city in the background is very consists of 10 buildings and there are park benches on the right side of the walkway. The ground is covered in snow and there is a pigeon standing in the middle of the walkway. The trees, there are trees in the background which all don't have leaves."}
+WHISPER_MODEL = MockModel()
+match os.environ.get('ENV_TYPE'):
+    case "dev":
+        pass
+    case "test":
+        WHISPER_MODEL = whisper.load_model('tiny.en')
+    case _:
+        WHISPER_MODEL = whisper.load_model('base.en')
 
 MEGA_CLIENT = Mega().login(
     os.environ.get('MEGA_USER'),
